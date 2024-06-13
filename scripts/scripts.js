109d92ee3733d4b9db70c19ecd96538676f26ade
Igor Araujo de Mattos <yigor88mattos@gmail.com>
2024-06-13 20:18:55 -0300

    Conserta função deleteUser()

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
-var userList = [];
+let userList = [];
 
+// Função para adicionar um novo usuário
 function addUser(firstName, lastName, email, phone, username, password) {
     const dataAtual = new Date();
     const dia = dataAtual.getDate().toString().padStart(2, '0'); // Dia do mês (1-31)
@@ -7,14 +8,24 @@ function addUser(firstName, lastName, email, phone, username, password) {
     const ano = dataAtual.getFullYear();
     const data = `${dia}/${mes}/${ano}`;
 
-    var newUser = { id: Math.random().toString(36).substr(2, 9), date: data, firstName: firstName, lastName: lastName, email: email, phone: phone, username: username, password: password };
+    let newUser = {
+        id: Math.random().toString(36).substr(2, 9),
+        date: data,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        phone: phone,
+        username: username,
+        password: password
+    };
     userList.push(newUser);
     localStorage.setItem('userList', JSON.stringify(userList));
     renderUserList();
 }
 
+// Função para deletar um usuário
 function deleteUser(userId) {
-    var updatedUserList = userList.filter(id => id !== userId);
+    let updatedUserList = userList.filter(user => user.id !== userId);
 
     if (updatedUserList.length < userList.length) {
         userList = updatedUserList;
@@ -27,36 +38,39 @@ function deleteUser(userId) {
 
 // Função para recuperar a lista de usuários do localStorage
 function getUserList() {
-    var storedList = JSON.parse(localStorage.getItem('userList'));
+    let storedList = JSON.parse(localStorage.getItem('userList'));
     userList = storedList || [];
 }
+
 // Função para renderizar a lista de usuários no HTML
 function renderUserList() {
-    var userListElement = document.getElementById('userList');
+    let userListElement = document.getElementById('userList');
     userListElement.innerHTML = ''; // Limpa o conteúdo HTML do elemento userListElement
 
     userList.forEach(function (user) {
-        var listItem = document.createElement('li');
+        let listItem = document.createElement('li');
         listItem.innerHTML = `<span class="user-date">(Data: ${user.date})</span>
                             <span class="user-email">(Email: ${user.email})</span>
-                            <span class="user-username">(Nome de Usuário: ${user.username})</span></span>
-                            <div><button class="delete-button" onclick="deleteUser(${user.id})">Excluir</button></div>`;
+                            <span class="user-username">(Nome de Usuário: ${user.username})</span>
+                            <div><button class="delete-button" onclick="deleteUser('${user.id}')">Excluir</button></div>`;
         userListElement.appendChild(listItem);
     });
 }
+
 // Recuperar a lista de usuários do localStorage
 getUserList();
 // Renderizar a lista de usuários no HTML
 renderUserList();
 
+// Event listener para o formulário de registro
 document.getElementById('registrationForm').addEventListener('submit', function (event) {
     event.preventDefault();
-    var firstNameInput = document.getElementById('firstName');
-    var lastNameInput = document.getElementById('lastName');
-    var emailInput = document.getElementById('email');
-    var phoneInput = document.getElementById('phone');
-    var usernameInput = document.getElementById('username');
-    var passwordInput = document.getElementById('password');
+    let firstNameInput = document.getElementById('firstName');
+    let lastNameInput = document.getElementById('lastName');
+    let emailInput = document.getElementById('email');
+    let phoneInput = document.getElementById('phone');
+    let usernameInput = document.getElementById('username');
+    let passwordInput = document.getElementById('password');
 
     addUser(firstNameInput.value, lastNameInput.value, emailInput.value, phoneInput.value, usernameInput.value, passwordInput.value);
 
@@ -68,8 +82,9 @@ document.getElementById('registrationForm').addEventListener('submit', function 
     passwordInput.value = '';
 });
 
+// Função para limpar a lista de usuários
 function limpaLista() {
-    var userListElement = document.getElementById('userList');
+    let userListElement = document.getElementById('userList');
     while (userListElement.firstChild) {
         userListElement.removeChild(userListElement.firstChild);
     }
