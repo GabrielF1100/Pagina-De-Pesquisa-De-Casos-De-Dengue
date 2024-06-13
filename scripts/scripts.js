6067be6d888f4e4a879500201514144ce7b1d836
GabrielF1100 <94715020+GabrielF1100@users.noreply.github.com>
2024-06-13 15:25:09 -0300

    Adiciona script.js

---
+++ scripts/scripts.js
@@ -0,0 +1,76 @@
+var userList = [];
+
+function addUser(firstName, lastName, email, phone, username, password) {
+    const dataAtual = new Date();
+    const dia = dataAtual.getDate().toString().padStart(2, '0'); // Dia do mês (1-31)
+    const mes = (dataAtual.getMonth() + 1).toString().padStart(2, '0'); // Mês (0-11, janeiro é 0, então somamos 1)
+    const ano = dataAtual.getFullYear();
+    const data = `${dia}/${mes}/${ano}`;
+
+    var newUser = { id: Math.random().toString(36).substr(2, 9), date: data, firstName: firstName, lastName: lastName, email: email, phone: phone, username: username, password: password };
+    userList.push(newUser);
+    localStorage.setItem('userList', JSON.stringify(userList));
+    renderUserList();
+}
+
+function deleteUser(userId) {
+    var updatedUserList = userList.filter(id => id !== userId);
+
+    if (updatedUserList.length < userList.length) {
+        userList = updatedUserList;
+        localStorage.setItem('userList', JSON.stringify(userList));
+        renderUserList();
+    } else {
+        alert('Usuário não encontrado.');
+    }
+}
+
+// Função para recuperar a lista de usuários do localStorage
+function getUserList() {
+    var storedList = JSON.parse(localStorage.getItem('userList'));
+    userList = storedList || [];
+}
+// Função para renderizar a lista de usuários no HTML
+function renderUserList() {
+    var userListElement = document.getElementById('userList');
+    userListElement.innerHTML = ''; // Limpa o conteúdo HTML do elemento userListElement
+
+    userList.forEach(function (user) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = `<span class="user-date">(Data: ${user.date})</span>
+                            <span class="user-email">(Email: ${user.email})</span>
+                            <span class="user-username">(Nome de Usuário: ${user.username})</span></span>
+                            <div><button class="delete-button" onclick="deleteUser(${user.id})">Excluir</button></div>`;
+        userListElement.appendChild(listItem);
+    });
+}
+// Recuperar a lista de usuários do localStorage
+getUserList();
+// Renderizar a lista de usuários no HTML
+renderUserList();
+
+document.getElementById('registrationForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var firstNameInput = document.getElementById('firstName');
+    var lastNameInput = document.getElementById('lastName');
+    var emailInput = document.getElementById('email');
+    var phoneInput = document.getElementById('phone');
+    var usernameInput = document.getElementById('username');
+    var passwordInput = document.getElementById('password');
+
+    addUser(firstNameInput.value, lastNameInput.value, emailInput.value, phoneInput.value, usernameInput.value, passwordInput.value);
+
+    firstNameInput.value = '';
+    lastNameInput.value = '';
+    emailInput.value = '';
+    phoneInput.value = '';
+    usernameInput.value = '';
+    passwordInput.value = '';
+});
+
+function limpaLista() {
+    var userListElement = document.getElementById('userList');
+    while (userListElement.firstChild) {
+        userListElement.removeChild(userListElement.firstChild);
+    }
+}
